017454221ff06d2bde2bd74db00160aad627479e
nedeljkovignjevic <vignjevic.nedeljko1999@gmail.com>
2022-01-11 20:27:16 +0100

    feat: Inital Spring Boot project.

---
+++ vaccineis-backend/src/main/java/vaccineisbackend/VaccineisBackendApplication.java
@@ -0,0 +1,13 @@
+package vaccineisbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccineisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccineisBackendApplication.class, args);
+	}
+
+}
---
+++ vaccineis-backend/src/test/java/vaccineisbackend/VaccineisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vaccineisbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccineisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
