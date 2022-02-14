11f815b7c5e41d2ad502630899a09d643b37bee0
nedeljkovignjevic <vignjevic.nedeljko1999@gmail.com>
2022-02-14 15:05:09 +0100

    refacor: Refactors.

--- vaccineis-backend/vaccineis-employee/src/main/java/vaccineisemployee/VaccineisEmployeeApplication.java
+++ vaccineis-backend/vaccineis-employee/src/main/java/vaccineisemployee/VaccineisEmployeeApplication.java
@@ -2,9 +2,10 @@ package vaccineisemployee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VaccineisEmployeeApplication {
+public class VaccineisEmployeeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VaccineisEmployeeApplication.class, args);
