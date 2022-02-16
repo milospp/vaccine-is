c609ea065649fb3261f0add54f72a83a7146c055
milospp <misa01523@gmail.com>
2022-02-17 00:57:06 +0100

    Fuseki crud

--- vaccineis-backend/vaccineis-portal/src/main/java/vaccineisportal/interesovanje/service/InteresovanjeService.java
+++ vaccineis-backend/vaccineis-portal/src/main/java/vaccineisportal/interesovanje/service/InteresovanjeService.java
@@ -15,20 +15,10 @@ public class InteresovanjeService {
 
     protected final FusekiManager fusekiManager;
     protected final ExistManager existManager;
+    protected final CRUDRDFRepository crudrdfRepository;
+
     public void writeSomething() {
-        Model model = ModelFactory.createDefaultModel();
-        model.setNsPrefix("pred", "http://www.ftn.uns.ac.rs/rdf/examples/predicate/");
-
-        Resource resource = model.createResource("http://www.ftn.uns.ac.rs/rdf/examples/person/Petar_Petrovic");
-
-        Property property1 = model.createProperty("http://www.ftn.uns.ac.rs/rdf/examples/predicate/","livesIn");
-        Literal literal1 = model.createLiteral("Novi Sad BREEE");
-
-        Statement statement = model.createStatement(resource, property1, literal1);
-
-        model.add(statement);
-
-//        Model model = fusekiManager.createRDFModel(statement);
-        fusekiManager.testUpload();
+        crudrdfRepository.uploadTriplet("test", "http://www.ftn.uns.ac.rs/rdf/examples/person/Petar_Petrovic", "radi", "voli");
+//        fusekiManager.testUpload();
     }
 }
